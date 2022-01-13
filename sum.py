0afe9a2d61262020715a8561d3e0dca48f9c5229
AshwaqALhakemi <81666805+AshwaqALhakemi@users.noreply.github.com>
2022-01-13 14:36:41 +0300

    CHALLENGE NO6

---
+++ sum.py
@@ -0,0 +1,17 @@
+def add(*x): # to add any numbers you want
+    return sum(x)
+
+print(add(2, 3))
+print(add(2, 3, 4))
+print(add(1, 2, 3, 4))
+
+
+
+def add( x, y, z=0): # to add tow or three numbers
+    return x + y + z
+
+
+# Driver co
+print(add(2, 3))
+print(add(2, 3, 4))
+
